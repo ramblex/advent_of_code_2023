c2dca51e369a49e450680b0112bf67414dc96b54
Alex Duller <alexd@afterthoughtsoftware.com>
2023-12-06 22:09:31 +0000

    Use quadratic equation to solve day 6!

--- lib/day06.ex
+++ lib/day06.ex
@@ -10,10 +10,7 @@ defmodule Day06 do
   def part1(filename) do
     read_input(filename, &number_list/1)
     |> Enum.map(fn {t, d} ->
-      Enum.filter(0..t, fn pause ->
-        pause * (t - pause) > d
-      end)
-      |> Enum.count
+      quadratic_gt0(t, d)
     end)
     |> Enum.reduce(1, fn x, acc -> x * acc end)
   end
@@ -29,12 +26,26 @@ defmodule Day06 do
   def part2(filename) do
     read_input(filename, &number_smush/1)
     |> Enum.map(fn {t, d} ->
-      Enum.filter(0..t, fn pause ->
-        pause * (t - pause) > d
-      end)
-      |> Enum.count
+      quadratic_gt0(t, d)
     end)
     |> Enum.reduce(1, fn x, acc -> x * acc end)
+  end
+
+  defp quadratic_gt0(t, d) do
+    # Equation is where n 0 -> t: n * (t - n) > d
+    # Rearranged to a quadratic:
+    # -(n^2) + tn - d
+
+    a = -1
+    b = t
+    c = -d
+
+    # Find where the curve crosses the x-axis:
+    lower_bound = (-b + :math.sqrt((b * b) - (4 * a * c))) / (2 * a)
+    upper_bound = (-b - :math.sqrt((b * b) - (4 * a * c))) / (2 * a)
+
+    # Number of whole integers between the two bounds:
+    :math.ceil(upper_bound) - :math.floor(lower_bound) - 1 |> trunc
   end
 
   defp read_input(filename, list_fun) do
